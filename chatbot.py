1afd0985b83511ddafd62a0abad69870ce1f1600
Elias Zamaria <mikez302@gmail.com>
2017-10-29 21:44:06 -0700

    Get coordinates from Google Geocoding API

--- chatbot.py
+++ chatbot.py
@@ -9,13 +9,34 @@ app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 
+class CoordinatesNotFoundError(Exception): pass
+
+
+def get_coordinates(location_str):
+    response = requests.get(
+        'https://maps.googleapis.com/maps/api/geocode/json',
+        params={'address': location_str, 'key': app.config['GOOGLE_GEOCODING_API_KEY']})
+    try:
+        location_result = response.json()['results'][0]
+    except (IndexError, KeyError) as exc:
+        raise CoordinatesNotFoundError from exc
+    coords = location_result['geometry']['location']
+    return (coords['lat'], coords['lng'])
+
+
 @app.route('/chat/messages', methods=['POST'])
 def handle_message():
     if request.form['action'] == 'message':
         for regex in [r'weather in ([\w\s-]+)', r'([\w\s-]+) weather']:
             matches = re.findall(regex, request.form['text'], re.IGNORECASE)
             if matches:
-                message_text = matches[0]
+                location_str = matches[0]
+                try:
+                    coords = get_coordinates(location_str)
+                except CoordinatesNotFoundError:
+                    message_text = f'Location “{location_str}” not found.'
+                else:
+                    message_text = str(coords)
                 break
         else:  # if location not found
             message_text = ('I didn\'t understand that. Enter something like ' +
