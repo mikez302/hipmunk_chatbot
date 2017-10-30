337fe247580bbdabe31024ef160cdf2f4e84fbe7
Elias Zamaria <mikez302@gmail.com>
2017-10-29 23:10:32 -0700

    Get weather from Dark Sky API

--- chatbot.py
+++ chatbot.py
@@ -13,6 +13,8 @@ class LocationNotFoundError(Exception): pass
 
 class CoordinatesNotFoundError(Exception): pass
 
+class WeatherNotFoundError(Exception): pass
+
 
 def location_from_query(query):
     for regex in [r'weather in ([\w\s-]+)', r'([\w\s-]+) weather']:
@@ -34,20 +36,39 @@ def get_coordinates(location_str):
     return (coords['lat'], coords['lng'])
 
 
+def get_weather(coordinates):
+    latitude, longitude = coordinates
+    key = app.config['DARK_SKY_API_KEY']
+    response = requests.get(f'https://api.darksky.net/forecast/{key}/{latitude},{longitude}')
+    weather = response.json()
+    try:
+        if 'temperature' in weather['currently'] and 'summary' in weather['currently']:
+            return weather['currently']
+        else:
+            raise WeatherNotFoundError()
+    except KeyError as exc:
+        raise WeatherNotFoundError from exc
+
+
 @app.route('/chat/messages', methods=['POST'])
 def handle_message():
     if request.form['action'] == 'message':
         try:
             location_str = location_from_query(request.form['text'])
             coords = get_coordinates(location_str)
+            current_weather = get_weather(coords)
         except LocationNotFoundError:
             message_text = ('I didn\'t understand that. Enter something like ' +
                             '“what\'s the weather in <Location>” or ' +
                             '“weather in <Location>” or “<Location> weather”.')
         except CoordinatesNotFoundError:
             message_text = f'Location “{location_str}” not found.'
+        except WeatherNotFoundError:
+            message_text = f'Couldn\'t get weather for {location_str}.'
         else:
-            message_text = str(coords)
+            temperature = round(current_weather['temperature'])
+            summary = current_weather['summary']
+            message_text = f'{temperature}°F. {summary}.'
     elif request.form['action'] == 'join':
         message_text = f'Hi {request.form["name"]}'
     else:
